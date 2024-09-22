2c309bea68d6689db5ff3734bb21fbe305b4e204
Parv <parvruhil68@gmail.com>
2024-09-22 10:49:17 +0530

    js file

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function loadContent(section) {
                     <img src="./images/avatar1.jpg" alt="Parv Avatar">
                     <h1>Hi👋 There, <br> I'm <span>Parv</span></h1>
                     <p>I am a passionate web developer with skills in HTML, CSS, and JavaScript.</p>
-                    <a href="#" class="cta-button">About Me</a>
+                    <a href="#" onClick="loadContent('about')" class="cta-button">About Me</a>
                     <div class="social-media">
                         <a href="https://github.com/parv68" target="_blank"><i class="fab fa-github"></i></a>
                         <a href="https://www.linkedin.com/in/parv-429659290/" target="_blank"><i class="fab fa-linkedin"></i></a>
@@ -20,20 +20,26 @@ function loadContent(section) {
             content.innerHTML = `
                 <div class="about">
                     <h2>About Me</h2>
-                    <p>I’m currently pursuing a Bachelor of Computer Application (BCA) and have a passion for web development. I love to build and design web applications that are both functional and aesthetically pleasing. With a focus on clean code and user experience, I’m excited to collaborate on projects that push the boundaries of what the web can do.</p>
+                    <p>I’m currently pursuing a Bachelor of Computer Application (BCA) and have a passion for <span>Frontend development</span>. I love to build and design web applications that are both functional and aesthetically pleasing. With a focus on clean code and user experience, I’m excited to collaborate on projects that push the boundaries of what the web can do.</p>
                 </div>
                 <div class="skills">
                     <h2>Skills</h2>
                     <ul>
-                        <li>HTML & CSS</li>
-                        <li>JavaScript & jQuery</li>
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>JavaScript</li>
+                        <li>jQuery</li>
                         <li>React.js (Basics)</li>
-                        <li>Node.js & Express (Basics)</li>
+                        <li>Tailwind CSS</li>
+                        <li>Bootstrap</li>
+                        <li>Git (Version Control)</li>
                     </ul>
                 </div>
                 <div class="education">
                     <h2>Education</h2>
-                    <p>Bachelor of Computer Application (BCA) - [Your University]</p>
+                    <p id="degree">Bachelor of Computer Application (BCA) - [Maharishi Dayanand University(MDU)]</p>
+                    <p id="address">Rohtak, Haryana</p>
+                    <p id="date">Aug 2023 - July 2026</p>
                 </div>
             `;
             break;
@@ -57,7 +63,7 @@ function loadContent(section) {
                         <div class="project-card">
                             <img src="./images/weather-app.png" alt="weather-app Image">
                             <h3>Project 2: Weather App</h3>
-                            <p>A weather app built with HTML, CSS, JavaScript, and API integration.</p>
+                            <p>A Weather app built with HTML, CSS, JavaScript, and API integration.</p>
                             <a href="https://parv68.github.io/weather-app/" target="_blank" class="project-link">View Project</a>
                             <br>
                             <a href="https://github.com/parv68/weather-app" target="_blank" class="project-link">View Code</a>
@@ -65,7 +71,7 @@ function loadContent(section) {
                         <div class="project-card">
                             <img src="./images/age-calculator.png" alt="age-calculator Image">
                             <h3>Project 3: Age calculator</h3>
-                            <p>An age calculator built with HTML, CSS and JavaScript.</p>
+                            <p>An Age calculator built with HTML, CSS and JavaScript.</p>
                             <a href="https://parv68.github.io/Age-calculator1/" target="_blank" class="project-link">View Project</a>
                             <br>
                             <a href="https://github.com/parv68/Age-calculator1" target="_blank" class="project-link">View Code</a>
@@ -73,7 +79,7 @@ function loadContent(section) {
                         <div class="project-card">
                             <img src="./images/bmi-calculator.png" alt="bmi-calculator Image">
                             <h3>Project 4: BMI Calculator</h3>
-                            <p>A weather app built with HTML, CSS and JavaScript.</p>
+                            <p>A BMI Calculator built with HTML, CSS and JavaScript.</p>
                             <a href="https://parv68.github.io/mass-calculator/" target="_blank" class="project-link">View Project</a>
                             <br>
                             <a href="https://github.com/parv68/mass-calculator" target="_blank" class="project-link">View Code</a>
@@ -81,7 +87,7 @@ function loadContent(section) {
                         <div class="project-card">
                             <img src="./images/food-delivery.png" alt="food delivery Image">
                             <h3>Project 5: Food Delivery</h3>
-                            <p>A weather app built with HTML and CSS.</p>
+                            <p>A Food delivery demo app built with HTML and CSS.</p>
                             <a href="https://parv68.github.io/food-delivery/" target="_blank" class="project-link">View Project</a>
                             <br>
                             <a href="https://github.com/parv68/food-delivery" target="_blank" class="project-link">View code</a>
@@ -89,7 +95,7 @@ function loadContent(section) {
                         <div class="project-card">
                             <img src="./images/password-generator.png" alt="password generator Image">
                             <h3>Project 6: Password Generator</h3>
-                            <p>A weather app built with HTML, CSS and JavaScript</p>
+                            <p>A Password generator built with HTML, CSS and JavaScript</p>
                             <a href="https://parv68.github.io/password-generator/" target="_blank" class="project-link">View Project</a>
                             <br>
                             <a href="https://github.com/parv68/password-generator" target="_blank" class="project-link">View Code</a>
@@ -97,7 +103,7 @@ function loadContent(section) {
                         <div class="project-card">
                             <img src="./images/text-to-speech.png" alt="Text to speech Image">
                             <h3>Project 7: Tetx to speech</h3>
-                            <p>A weather app built with HTML, CSS and JavaScript.</p>
+                            <p>A Text to speech built with HTML, CSS and JavaScript.</p>
                             <a href="https://parv68.github.io/text-to-speech/" target="_blank" class="project-link">View Project</a>
                             <br>
                             <a href="https://github.com/parv68/text-to-speech" target="_blank" class="project-link">View Code</a>
@@ -105,7 +111,7 @@ function loadContent(section) {
                         <div class="project-card">
                             <img src="./images/speech-to-text.png" alt="Speech to text Image">
                             <h3>Project 8: Speech to text</h3>
-                            <p>A weather app built with HTML, CSS and JavaScript.</p>
+                            <p>A Speech to text built with HTML, CSS and JavaScript.</p>
                             <a href="https://parv68.github.io/speech-to-text/" target="_blank" class="project-link">View Project</a>
                             <br>
                             <a href="https://github.com/parv68/speech-to-text" target="_blank" class="project-link">View Code</a>
@@ -113,7 +119,7 @@ function loadContent(section) {
                         <div class="project-card">
                             <img src="./images/music-player.png" alt="Music player Image">
                             <h3>Project 9: Music Player</h3>
-                            <p>A weather app built with HTML, CSS and JavaScript.</p>
+                            <p>A Music player built with HTML, CSS and JavaScript.</p>
                             <a href="https://github.com/parv68/music-player" target="_blank" class="project-link">View Project</a>
                             <br>
                             <a href="https://parv68.github.io/music-player/" target="_blank" class="project-link">View Code</a>
@@ -121,7 +127,7 @@ function loadContent(section) {
                         <div class="project-card">
                             <img src="./images/todo-app.png" alt="Todo app Image">
                             <h3>Project 10: Todo app</h3>
-                            <p>A weather app built with HTML, CSS, JavaScript, and API integration.</p>
+                            <p>A simple todo app built with HTML, CSS, JavaScript, and API integration.</p>
                             <a href="https://github.com/parv68/todo-app" target="_blank" class="project-link">View Project</a>
                             <br>
                             <a href="https://parv68.github.io/todo-app/" target="_blank" class="project-link">View Code</a>
@@ -130,7 +136,7 @@ function loadContent(section) {
                     </div>
 
                     <div class="button">
-                        <a href="#" class="cta-button">View All Projects</a>
+                        <a href="https://github.com/parv68?tab=repositories" class="cta-button" target="_blank">View All Projects</a>
                     </div>
                 </div>
                 `;
